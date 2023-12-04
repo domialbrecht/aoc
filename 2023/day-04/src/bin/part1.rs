5b1a7e4ba79ced6efc4ce8c4feaf73bcbf4bd493
dominika <dominik.albrecht@intersim.ch>
2023-12-04 20:36:57 +0100

    Add day04 part1

--- 2023/day-04/src/bin/part1.rs
+++ 2023/day-04/src/bin/part1.rs
@@ -6,8 +6,53 @@ fn main() {
     println!("Result: {}", process(&contents))
 }
 
+#[derive(Debug)]
+struct Scratchcard<'a> {
+    id: u32,
+    winnings: &'a str,
+    mine: &'a str,
+}
+
+impl<'a> Scratchcard<'a> {
+    fn score(&self) -> u32 {
+        let matches = self.mine.split_whitespace().fold(0, |acc, res| {
+            acc + if self
+                .winnings
+                .split_whitespace()
+                .collect::<Vec<&str>>()
+                .contains(&res)
+            {
+                1
+            } else {
+                0
+            }
+        });
+        if matches > 0 {
+            2u32.pow(matches - 1)
+        } else {
+            0
+        }
+    }
+}
+
 fn process(games: &str) -> u32 {
-    todo!()
+    games
+        .lines()
+        .map(|line| {
+            let (game, scratches) = line
+                .split_once(':')
+                .expect("should be able to split away game");
+            let (winning, mine) = scratches
+                .split_once('|')
+                .expect("should have winning and mine split");
+            let game: String = game.chars().filter(|c| c.is_ascii_digit()).collect();
+            Scratchcard {
+                id: game.parse().unwrap(),
+                winnings: winning.trim(),
+                mine: mine.trim(),
+            }
+        })
+        .fold(0, |acc, card| acc + card.score())
 }
 
 #[cfg(test)]
@@ -17,6 +62,6 @@ mod tests {
     #[test]
     fn test_game() {
         let contents = fs::read_to_string("input-test.txt").expect("Should read file");
-        assert_eq!(process(&contents), 4361);
+        assert_eq!(process(&contents), 13);
     }
 }
