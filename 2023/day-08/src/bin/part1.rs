1fd82efc4a6004454b3762977c66bcdffa11a9ea
dominika <dominik.albrecht@intersim.ch>
2023-12-10 19:51:32 +0100

    Finish day08

--- 2023/day-08/src/bin/part1.rs
+++ 2023/day-08/src/bin/part1.rs
@@ -3,7 +3,7 @@ use std::{collections::BTreeMap, fs};
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{self, alpha1, line_ending, multispace1},
+    character::complete::{self, alpha1, alphanumeric1, line_ending, multispace1},
     combinator::eof,
     multi::{fold_many1, many1},
     sequence::{delimited, separated_pair, terminated},
@@ -33,11 +33,11 @@ fn parser(input: &str) -> IResult<&str, (Vec<Direction>, BTreeMap<&str, (&str, &
     let (input, map) = fold_many1(
         terminated(
             separated_pair(
-                alpha1,
+                alphanumeric1,
                 tag(" = "),
                 delimited(
                     complete::char('('),
-                    separated_pair(alpha1, tag(", "), alpha1),
+                    separated_pair(alphanumeric1, tag(", "), alphanumeric1),
                     complete::char(')'),
                 ),
             ),
@@ -56,29 +56,56 @@ fn parser(input: &str) -> IResult<&str, (Vec<Direction>, BTreeMap<&str, (&str, &
 #[tracing::instrument]
 fn process(contents: &str) -> usize {
     let (_, (instructions, map)) = parser(contents).expect("should validly parse");
-    let mut node = "AAA";
-    let Some(step_count) =
-        instructions
-            .iter()
-            .cycle()
-            .enumerate()
-            .find_map(|(index, instruction)| {
-                let options = map.get(node).expect("always have valid node");
-                let next_node = match instruction {
-                    Direction::Left => options.0,
-                    Direction::Right => options.1,
-                };
-                if next_node == "ZZZ" {
-                    Some(index + 1)
-                } else {
-                    node = next_node;
-                    None
-                }
-            })
-    else {
-        panic!("infinite error")
-    };
-    step_count
+    let nodes = map.keys().cloned().filter(|key| key.ends_with('A'));
+
+    let step_counts: Vec<usize> = nodes
+        .map(|mut node| {
+            let Some(step_count) =
+                instructions
+                    .iter()
+                    .cycle()
+                    .enumerate()
+                    .find_map(|(index, instruction)| {
+                        let options = map.get(node).expect("always have valid node");
+                        let next_node = match instruction {
+                            Direction::Left => options.0,
+                            Direction::Right => options.1,
+                        };
+                        if next_node.ends_with('Z') {
+                            Some(index + 1)
+                        } else {
+                            node = next_node;
+                            None
+                        }
+                    })
+            else {
+                panic!("infinite error")
+            };
+            step_count
+        })
+        .collect();
+    //Get lcm of all step counts
+    let mut result = step_counts[0];
+    step_counts
+        .iter()
+        .enumerate()
+        .skip(1)
+        .for_each(|(index, _)| {
+            result = lcm(result, step_counts[index]);
+        });
+    result
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
 }
 
 #[cfg(test)]
@@ -86,19 +113,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_game() {
-        let contents = fs::read_to_string("input-test.txt").expect("Should read file");
-        assert_eq!(process(&contents), 2);
-    }
-
-    #[test]
     fn test_game_2() {
-        let contents = "LLR
+        let contents = "LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)
-";
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX";
         assert_eq!(process(contents), 6);
     }
 }
