0e1fa38db7c48765716727ff439c7f309bea5f4c
dominika <dominik.albrecht@intersim.ch>
2023-12-15 18:53:19 +0100

    Day10 progress

--- 2023/day-10/src/bin/part1.rs
+++ 2023/day-10/src/bin/part1.rs
@@ -1,13 +1,63 @@
-use std::fs;
+use std::{collections::HashMap, fs};
+
+use day10::{input_to_grid, Graph, Node};
 
 fn main() {
-    let contents = fs::read_to_string("./day-10/input.txt").expect("Should read file");
+    let contents = fs::read_to_string("./day-10/input-test.txt").expect("Should read file");
     println!("=========");
     println!("Result: {}", process(&contents))
 }
-
+// .....
+// .S-7.
+// .|.|.
+// .L-J.
+// .....
+//
+// ..F7.
+// .FJ|.
+// SJ.L7
+// |F--J
+// LJ...
 fn process(content: &str) -> u64 {
-    todo!()
+    create_graph(content)
+}
+
+fn create_graph(content: &str) -> u64 {
+    let grid = input_to_grid(content);
+    let edges: HashMap<Node, Vec<Node>> = grid
+        .iter()
+        .map(|node| {
+            let node_neighbors = neighboring_edges(node.value);
+            let neighbors: Vec<Node> = node_neighbors
+                .iter()
+                .filter_map(|next_pos| {
+                    let pos_next_node = Node {
+                        position: (node.position.0 + next_pos.0, node.position.1 + next_pos.1),
+                        value: '.',
+                    };
+                    grid.get(&pos_next_node).cloned()
+                })
+                .collect();
+            (*node, neighbors)
+        })
+        .filter(|(_, neighbors)| !neighbors.is_empty())
+        .collect();
+    dbg!(edges);
+    1
+}
+
+fn neighboring_edges(me: char) -> Vec<(i32, i32)> {
+    match me {
+        '|' => vec![(0, 1), (0, -1)],
+        '-' => vec![(1, 0), (-1, 0)],
+        'L' => vec![(0, 1), (1, 0)],
+        'J' => vec![(0, 1), (-1, 0)],
+        '7' => vec![(0, -1), (-1, 0)],
+        'F' => vec![(0, -1), (1, 0)],
+        'S' => vec![(0, 1), (1, 0), (-1, 0), (-1, 0)],
+        '.' => vec![],
+        x => panic!("Invalid gridType {}", x),
+    }
 }
 
 #[cfg(test)]
@@ -22,7 +72,7 @@ mod tests {
 .L-J.
 .....
 ";
-        assert_eq!(process(&contents), 4);
+        assert_eq!(process(contents), 4);
     }
 
     #[test]
@@ -32,6 +82,6 @@ mod tests {
 SJ.L7
 |F--J
 LJ...";
-        assert_eq!(process(&contents), 8);
+        assert_eq!(process(contents), 8);
     }
 }
--- 2023/day-10/src/lib.rs
+++ 2023/day-10/src/lib.rs
@@ -1,3 +1,63 @@
-struct Node {
-    position: (i32, i32),
+use std::collections::{HashMap, HashSet, VecDeque};
+
+pub fn input_to_grid(content: &str) -> HashSet<Node> {
+    content
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().map(move |(x, char)| Node {
+                position: (
+                    x.try_into().expect("convert z to i32"),
+                    y.try_into().expect("convert y to i32"),
+                ),
+                value: char,
+            })
+        })
+        .collect()
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
+pub struct Node {
+    pub position: (i32, i32),
+    pub value: char,
+}
+
+// { "A", { "B" } },
+// { "B", { "A", "C", "D" } },
+// { "C", { "A" } },
+// { "D", { "E", "A" } },
+// { "E", { "B" } }
+pub struct Graph {
+    edges: HashMap<Node, Vec<Node>>,
+}
+
+impl Graph {
+    pub fn bfs(&self, start: Node) -> HashMap<Node, Option<Node>> {
+        let mut frontier = VecDeque::new();
+        frontier.push_front(start);
+
+        let mut came_from: HashMap<Node, Option<Node>> = HashMap::new();
+        came_from.insert(start, None);
+        while !frontier.is_empty() {
+            if let Some(current) = frontier.pop_front() {
+                println!("Visiting {:?}", &current);
+                self.neighbors(current).iter().for_each(|next| {
+                    if came_from.get(next).is_none() {
+                        frontier.push_front(*next);
+                        came_from.insert(*next, Some(current));
+                    }
+                })
+            }
+        }
+
+        came_from
+    }
+
+    pub fn bfs_longest(&self, start: Node) -> usize {
+        todo!()
+    }
+
+    fn neighbors(&self, node: Node) -> Vec<Node> {
+        return self.edges.get(&node).unwrap().clone();
+    }
 }
