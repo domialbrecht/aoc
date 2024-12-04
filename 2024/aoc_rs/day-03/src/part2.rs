8d2d4f7462ff94a5451c78e329052f6c7d13d8a0
dominika <dominik.albrecht@intersim.ch>
2024-12-04 20:27:57 +0100

    we call this done

--- 2024/aoc_rs/day-03/src/part2.rs
+++ 2024/aoc_rs/day-03/src/part2.rs
@@ -1,6 +1,37 @@
+use miette::miette;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, anychar},
+    multi::{many1, many_till},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
+};
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let (_, instructions) = parse(input).map_err(|e| miette!("parse failed {}", e))?;
+    let result: u32 = instructions.iter().map(|inst| inst.0 * inst.1).sum();
+    Ok(result.to_string())
+}
+
+type Instruction = (u32, u32);
+
+// Change this to mul_instruction
+// Implement different instructions
+// Return instruction type along instruction
+fn instruction(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("mul")(input)?;
+    let (input, pair) = delimited(
+        tag("("),
+        separated_pair(complete::u32, tag(","), complete::u32),
+        tag(")"),
+    )(input)?;
+    Ok((input, pair))
+}
+
+// Use alt to match all instructions
+fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
+    many1(many_till(anychar, instruction).map(|(_rest, inst)| inst))(input)
 }
 
 #[cfg(test)]
@@ -9,9 +40,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!("48", process(input)?);
         Ok(())
     }
 }
