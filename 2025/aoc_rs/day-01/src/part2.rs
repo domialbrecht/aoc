852e1510efbf8e127f6dab54c258376cf92d22a8
dominika <dominik.albrecht@intersim.ch>
2025-12-01 20:01:34 +0100

    feat: 2025 day01 part2

--- 2025/aoc_rs/day-01/src/part2.rs
+++ 2025/aoc_rs/day-01/src/part2.rs
@@ -1,6 +1,37 @@
+use std::ops::Div;
+
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+    let instructions: Vec<i32> = _input
+        .lines()
+        .map(|line| {
+            let mut chars = line.chars();
+            let dir = chars.next().unwrap();
+            let amount: i32 = chars.as_str().trim().parse().unwrap();
+
+            if let 'L' = dir {
+                -amount
+            } else {
+                amount
+            }
+        })
+        .collect();
+
+    let mut dial = 50;
+    let mut result = 0;
+
+    for steps in instructions {
+        let next = dial + steps;
+        let mut arounds = next.div(100).abs();
+        if dial != 0 && next <= 0 {
+            arounds += 1;
+        }
+
+        dial = next.rem_euclid(100);
+        result += arounds;
+    }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +40,42 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        assert_eq!("6", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_process_half() -> miette::Result<()> {
+        let input = "L68
+L30
+R48
+L5
+R60";
+        assert_eq!("3", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_process_big() -> miette::Result<()> {
+        let input = "L1000";
+        assert_eq!("10", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_process_right() -> miette::Result<()> {
+        let input = "R65";
+        assert_eq!("1", process(input)?);
         Ok(())
     }
 }
