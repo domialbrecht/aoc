5ca211c26f61a5e122f63213631870c9418a61d9
dominika <dominik.albrecht@intersim.ch>
2025-12-03 18:09:06 +0100

    feat: 2025 day03 part2

--- 2025/aoc_rs/day-03/src/part2.rs
+++ 2025/aoc_rs/day-03/src/part2.rs
@@ -1,6 +1,37 @@
+use itertools::Itertools;
+
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+    let result = _input
+        .lines()
+        .map(|bank| {
+            let bank = bank.trim();
+            let mut batteries: Vec<char> = vec![];
+            let mut current_index = 0;
+            for i in 0..11 {
+                let res = &bank[current_index..(bank.len() - 11 + i)]
+                    .chars()
+                    .enumerate()
+                    .max_set_by_key(|(_i, battery)| *battery);
+                let (index, max_start) = res.first().unwrap();
+
+                batteries.push(*max_start);
+                current_index += index + 1;
+            }
+
+            let (_, max_end) = &bank[(current_index)..]
+                .chars()
+                .enumerate()
+                .max_by_key(|(_i, battery)| *battery)
+                .unwrap();
+
+            batteries.push(*max_end);
+
+            batteries.iter().collect::<String>().parse::<u64>().unwrap()
+        })
+        .sum::<u64>();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +40,18 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        assert_eq!("3121910778619", process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_process_9() -> miette::Result<()> {
+        let input = "999999999999111";
+        assert_eq!("999999999999", process(input)?);
         Ok(())
     }
 }
