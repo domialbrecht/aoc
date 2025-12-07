2c43a2d164c4ced26b3afb0c390097737b03567f
dominika <dominik.albrecht@intersim.ch>
2025-12-07 12:01:29 +0100

    feat: 2025 day06 part1

--- 2025/aoc_rs/day-06/src/part1.rs
+++ 2025/aoc_rs/day-06/src/part1.rs
@@ -1,6 +1,30 @@
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+    let sets: Vec<Vec<&str>> = _input
+        .lines()
+        .map(|l| l.split_whitespace().map(|s| s.trim()).collect())
+        .collect();
+
+    let (ops, nums) = sets.split_last().unwrap();
+    dbg!(&nums);
+
+    let result: u64 = ops
+        .iter()
+        .enumerate()
+        .map(|(col_i, op)| {
+            let num_it = (0..nums.len()).map(|row_i| nums[row_i][col_i].parse::<u64>().unwrap());
+
+            match *op {
+                "*" => num_it.product::<u64>(),
+                "+" => num_it.sum(),
+                _ => {
+                    panic!("op match error")
+                }
+            }
+        })
+        .sum();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +33,11 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  ";
+        assert_eq!("4277556", process(input)?);
         Ok(())
     }
 }
